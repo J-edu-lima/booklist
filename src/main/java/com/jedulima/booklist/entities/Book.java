74bd4c751a08a9c70b8b01073a78fe7913d2ff63
José Eduardo <limaedu18@hotmail.com>
2024-11-13 22:39:18 -0300

    Entidade principal e configuração do banco de dados

---
+++ src/main/java/com/jedulima/booklist/entities/Book.java
@@ -0,0 +1,26 @@
+package com.jedulima.booklist.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.io.Serializable;
+
+@Entity
+@Table(name = "tb_book")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+public class Book implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    @Column(name = "book_year")
+    Integer year;
+    String genre;
+    String imgUrl;
+    String shortDescription;
+}
